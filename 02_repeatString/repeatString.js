0a093dd343d4d28664cbdb2b6e2dea392c59d083
Jason <jasonhuggins11@outlook.com>
2022-07-25 00:55:06 +0100

    Add function body

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let text = string;
 
+    for (let i = 1; i < num; i++) {
+        text += string;
+    }
+
+    return text;
 };
 
 // Do not edit below this line
