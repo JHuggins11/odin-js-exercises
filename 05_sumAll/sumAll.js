cfb5d3db4b7a42baf364368e20076a49f275b880
Jason <jasonhuggins11@outlook.com>
2022-07-25 02:41:36 +0100

    Add for loop to do sum requested in README

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    for (let i = start; i <= end; i++) {
+        //console.log(`Current: ${i}`);
+
+        sum += i;
+        //console.log(`Current sum: ${sum}`);
+    }
+
+    return sum;
 };
+
+//console.log(`Final sum: ${sumAll(1, 4)}`);
 
 // Do not edit below this line
 module.exports = sumAll;
