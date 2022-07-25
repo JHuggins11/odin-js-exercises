fe9670d83e644b80faefcd312255ab830563ccfa
Jason <jasonhuggins11@outlook.com>
2022-07-25 01:40:36 +0100

    Add function body to remove single element

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item) {    
+    let index = array.indexOf(item);
+    let deletedItems = array.splice(index, 1);
+    
+    return array;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
