aed3205c2a31c4d9c0feae54b20b94be9fbb1abe
Jason <jasonhuggins11@outlook.com>
2022-07-25 03:03:32 +0100

    Alter if statement to check if args aren't numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,10 +2,7 @@ const sumAll = function(num1, num2) {
     let start, end;
     let sum = 0;
 
-    //console.log(typeof(num1) === "string");
-    //console.log(typeof(num2) === "string");
-
-    if ((typeof(num1)) === "string" || (typeof(num2) === "string") 
+    if ((typeof(num1) != "number") || (typeof(num2) != "number") 
         || (num1 < 0) || (num2 < 0)) {
         return "ERROR";
     }
@@ -19,16 +16,11 @@ const sumAll = function(num1, num2) {
     }
 
     for (let i = start; i <= end; i++) {
-        //console.log(`Current: ${i}`);
-
         sum += i;
-        //console.log(`Current sum: ${sum}`);
     }
 
     return sum;
 };
-
-//console.log(`Final sum: ${sumAll(10, "90")}`);
 
 // Do not edit below this line
 module.exports = sumAll;
