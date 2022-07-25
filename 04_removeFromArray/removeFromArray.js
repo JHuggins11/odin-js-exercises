3a216101bf0ed2ad25fca15abae3264a1ec5c087
Jason <jasonhuggins11@outlook.com>
2022-07-25 02:11:28 +0100

    Skip items that aren't in the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -4,17 +4,22 @@ const removeFromArray = function(array, ...items) {
     //console.log(items);
 
     for (const value of items) {
-        //console.log(value);
-        let index = array.indexOf(value);
-        //console.log(index);
-        array.splice(index, 1);
+        if (!array.includes(value)) {
+            continue;
+        }
+        else {
+            //console.log(value);
+            let index = array.indexOf(value);
+            //console.log(index);
+            array.splice(index, 1);
+        }
     }    
     //console.log(array);
 
     return array;
 };
 
-//removeFromArray([1, 2, 3, 4], 2, 3, 4);
+//removeFromArray([1, 2, 3, 4], 7, "taco");
 
 // Do not edit below this line
 module.exports = removeFromArray;
