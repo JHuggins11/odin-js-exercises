3558b04597b9d13a1b59d9bca30aa1e7e371643d
Jason <jasonhuggins11@outlook.com>
2022-08-05 20:19:10 +0100

    Write function to check arg is palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    let reversedString = "";
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+
+    return (reversedString === string) ? true : false;
 };
 
 // Do not edit below this line
