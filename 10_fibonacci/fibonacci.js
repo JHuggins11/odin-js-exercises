1245df86001f347af1e83c5298a49ed519289023
Jason <jasonhuggins11@outlook.com>
2022-08-05 21:20:14 +0100

    Add function to calc basic fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    const sequence = [0, 1];
+    
+    for (let i = 2; i <= index; i++) {
+        let a = sequence[i - 1];
+        let b = sequence[i - 2];
+        sequence.push(a + b);
+    }
 
+    return sequence[index];
 };
 
 // Do not edit below this line
