c16ff37c6b4a66a37aba0e9a9ed6bc391ce604f4
Jason <jasonhuggins11@outlook.com>
2022-08-05 20:06:29 +0100

    Write function to calculate factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -36,8 +36,19 @@ const power = function(base, exp) {
   return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = num;
+
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  while (num > 1) {
+    result *= num;
+    num--;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
