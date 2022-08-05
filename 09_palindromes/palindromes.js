cca31f2d46e8a266d628ca8adc6f8dd625b7c4a1
Jason <jasonhuggins11@outlook.com>
2022-08-05 20:40:40 +0100

    Add regex for removing non-letters from string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
 const palindromes = function(string) {
     let reversedString = "";
+    // Remove characters that aren't letters and set to lowercase
+    string = string.replace(/[^a-z]/g, "").toLowerCase();
 
     for (let i = string.length - 1; i >= 0; i--) {
         reversedString += string[i];
