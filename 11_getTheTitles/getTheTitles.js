e96e37a961a59bbd17a2b08c0b737065d8af47a8
Jason <jasonhuggins11@outlook.com>
2022-08-05 21:50:20 +0100

    Return all book titles in new array with map()

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArray) {
+    return bookTitles = bookArray.map((author) => author.title);
 };
 
 // Do not edit below this line
