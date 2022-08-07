9921042f9a4c0e72ea1881d09e96b45676f3db3f
Jason <jasonhuggins11@outlook.com>
2022-08-07 23:14:46 +0100

    Attempt function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    people.reduce((oldestPerson, currentPerson) => {
+        if (oldestPerson.yearOfDeath === undefined) {
+            oldestPerson.yearOfDeath = currentYear;
+        }
+        else if (currentPerson.yearOfDeath === undefined) {
+            currentPerson.yearOfDeath = currentYear;
+        }
+        
+        let oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        let currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+
+        return currentAge > oldestAge ? currentPerson : oldestPerson;
+    });
 };
+
+// See solution in solutions branch
 
 // Do not edit below this line
 module.exports = findTheOldest;
